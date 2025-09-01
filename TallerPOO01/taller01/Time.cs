61daf69ca8b91ec81b568b585ed29cd09d73776d
MOISES DE JESUS GONZALEZ NAVARRO <moisesgonzalez1140340@correo.itm.edu.co>
2025-09-01 15:17:25 -0500

    Set Builders and Atributes

--- TallerPOO01/taller01/Time.cs
+++ TallerPOO01/taller01/Time.cs
@@ -8,6 +8,104 @@ namespace taller01
 {
     internal class Time
     {
-        return 0;
+        // Camps
+        private int _hour;
+        private int _minute;
+        private int _second;
+        private int _millisecond;
+
+        // builders
+        public Time ()
+        {
+            _hour = 0;
+            _minute = 0;
+            _second = 0;
+            _millisecond = 0;
+        }
+        public Time (int hour)
+        {
+            _hour = hour;
+        }
+
+        public Time (int hour, int minute)
+        {
+            _hour = hour;
+            _minute = minute;
+        }
+
+        public Time (int hour, int minute, int second)
+        {
+            _hour = hour;
+            _minute = minute;
+            _second = second;
+        }
+
+        public Time (int hour, int minute, int second, int milliseconds)
+        {
+            _hour = hour;
+            _minute = minute;
+            _second = second;
+            _millisecond = milliseconds;
+        }
+
+        //Atributes
+        public int Hour
+        {
+            get => _hour;
+            set
+            {
+                Hour = ValidHour ( value );
+            }
+        }
+
+        public int Minute
+        {
+            get => _minute;
+            set
+            {
+                Minute = ValidMinute(value);
+            }
+        }
+
+        public int second
+        {
+            get => _second;
+            set
+            {
+                second = ValidSecond(value);
+            }
+        }
+
+
+        public int Millisecond
+        {
+            get => _millisecond;
+            set
+            {
+                Millisecond = ValidMillisecond(value);
+            }
+        }
+
+
+        private int ValidSecond(int value)
+        {
+            throw new NotImplementedException();
+        }
+        private int ValidMillisecond(int value)
+        {
+            throw new NotImplementedException();
+        }
+
+        private int ValidMinute(int value)
+        {
+            throw new NotImplementedException();
+        }
+
+        private int ValidHour(int value)
+        {
+            throw new NotImplementedException();
+        }
     }
-}
+
+       
+    }
